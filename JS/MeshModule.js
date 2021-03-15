a88f201c9862da52583467399d81082d0a389234
Erin cooper <36458674+ErinTheSmall@users.noreply.github.com>
2021-03-15 18:10:09 +0000

    re implement dolly zoom check because.. it was needed

--- JS/MeshModule.js
+++ JS/MeshModule.js
@@ -261,6 +261,10 @@ function viewer() {
                 let zoomScale = Math.pow(0.95, amount * cameraControlsPerspective.dollySpeed);//calculate equivelant zoom
                 let dollyDistance = cameraControlsPerspective.distance * dollyScale;
                 let zoomAmount = cameraOrthographic.zoom * zoomScale;
+                if (dollyDistance > cameraControlsOrthographic.minDistance && dollyDistance < cameraControlsOrthographic.maxDistance) { // check if dolly is too far in or out
+                    cameraControlsPerspective.dollyTo(dollyDistance, true);
+                    cameraControlsOrthographic.zoomTo(zoomAmount, true);
+                };
 
             };
 
